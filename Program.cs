8f19129802dadf45b0b2e388d598fd768398fe5a
tdy2012 <tdy2020@gmail.com>
2020-03-07 20:17:26 +0700

    [Master] Initialize C# project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mdt112_assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
